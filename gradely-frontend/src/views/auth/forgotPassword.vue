a707e36a54d826bad0c818d7d1ad2e6779448c30
michealarumgbo <arumgbomicheal@gmail.com>
2025-09-17 12:33:59 +0100

    modified forgotPassword added changePassword and check email pages

--- gradely-frontend/src/views/auth/forgotPassword.vue
+++ gradely-frontend/src/views/auth/forgotPassword.vue
@@ -1,8 +1,22 @@
 <template>
     <div class="w-full h-[100vh] flex items-center justify-center bg-[#f9fafb]">
-       <div class="w-[50%] h-[70%] bg-white shadow flex items-center">
-        <h3>Recover Account</h3>
-       </div>
+        <div class="w-[80%] h-[70%] bg-white shadow-md flex flex-col md:flex-row items-center ">
+            <div class="w-full h-[45%] md:h-[100%] md:w-[50%] bg-no-repeat  bg-cover bg-center"
+                style="background-image: url('/src/assets/images/forgotpassword.png');"></div>
+            <form action="#"
+                class="flex flex-col w-full md:w-[50%] md:h-full md:pt-[30px] pt-[10px] px-[20px] md:px-[70px]">
+                <img src="/src/assets/images/logo/GradelyLogo.png" class="w-[120px] hidden md:block" alt="">
+                <h3 class="text-[25px] font-[500] leading-[40px] md:text-[40px]">Forgot your Password?</h3>
+                <p class="text-xs mb-[10px] md:text-[16px] md:mb-[15px] text-gray-700">Enter email address to reset
+                    password</p>
+                <input type="email"
+                    class="border h-[40px] pl-[10px] text-[16px] rounded-sm border-gray-400 focus-within:border-[#09193e] outline-none mb-[10px] md:mb-[20px]"
+                    name="recovery-email" placeholder="Enter Email" id="recovery-email">
+                <button type="submit"
+                    class="cursor-pointer bg-[#09193e] rounded-sm h-[40px] font-[500] text-white text-[15px]">Get
+                    Link</button>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -10,6 +24,4 @@
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
