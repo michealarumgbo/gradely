b62c72bcabe98586fbf9afea76839c0902a0a6a5
michealarumgbo <arumgbomicheal@gmail.com>
2025-09-17 10:17:00 +0100

    Added changePassword page, forgot password image

--- gradely-frontend/src/views/auth/forgotPassword.vue
+++ gradely-frontend/src/views/auth/forgotPassword.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        forgotpassword
+    <div class="w-full h-[100vh] flex items-center justify-center bg-[#f9fafb]">
+       <div class="w-[50%] h-[70%] bg-white shadow flex items-center">
+        <h3>Recover Account</h3>
+       </div>
     </div>
 </template>
 
